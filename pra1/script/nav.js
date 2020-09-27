65844c4f6e09f1de8c647e04a0e447ee68036c5e
fromRK <access.iy.business@gmail.com>
2020-09-27 20:54:01 +0900

    20200927 jquery練習

---
+++ pra1/script/nav.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.drawer').drawer();
+});
+
+$(function(){
+$('.slick').slick({
+    dots:true,
+});
+});
+
+$(function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $(',js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
